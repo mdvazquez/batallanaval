d8328b9495875ad843e7b140c262791bbb1a3796
mdvazquez <maxiv_833@hotmail.com>
2018-04-06 17:17:45 -0300

    inicializacion del proyecto

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 4, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+				<h1>
+					<div>
+						Batalla Naval CURZA
+					</div>
+				</h1>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<ul class="nav">
+				<li class="nav-item">
+					<a class="nav-link active" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Profile</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#">Messages</a>
+				</li>
+				<li class="nav-item dropdown ml-md-auto">
+					 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+						 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+						<div class="dropdown-divider">
+						</div> <a class="dropdown-item" href="#">Separated link</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>
+							#
+						</th>
+						<th>
+							Barco
+						</th>
+						<th>
+							Coordenadas
+						</th>
+						<th>
+							Orientacion
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							1
+						</td>
+						<td>
+							Barco 1
+						</td>
+						<td>
+							<input name="nombredelacaja" size="2" type="text">
+							<input name="nombredelacaja" size="2" type="text">
+						</td>
+						<td>
+							<select name="OS">
+								 
+								<option value="1">
+									Horizontal
+								</option> 
+								<option value="2">
+									Vertical
+								</option>
+							</select>
+						</td>
+					</tr>
+					<tr class="table-active">
+						<td>
+							2
+						</td>
+						<td>
+							Barco 2
+						</td>
+						<td>
+							<input name="nombredelacaja" size="2" type="text">
+							<input name="nombredelacaja" size="2" type="text">
+						</td>
+						<td>
+							<select name="OS">
+								 
+								<option value="1">
+									Horizontal
+								</option> 
+								<option value="2">
+									Vertical
+								</option>
+							</select>
+						</td>
+					</tr>
+					<tr class="table-success">
+						<td>
+							3
+						</td>
+						<td>
+							Barco 3
+						</td>
+						<td>
+							<input name="nombredelacaja" size="2" type="text">
+							<input name="nombredelacaja" size="2" type="text">
+						</td>
+						<td>
+							<select name="OS">
+								 
+								<option value="1">
+									Horizontal
+								</option> 
+								<option value="2">
+									Vertical
+								</option>
+							</select>
+						</td>
+					</tr>
+					<tr class="table-warning">
+						<td>
+							4
+						</td>
+						<td>
+							Barco 4
+						</td>
+						<td>
+							<input name="nombredelacaja" size="2" type="text">
+							<input name="nombredelacaja" size="2" type="text">
+						</td>
+						<td>
+							<select name="OS">
+								 
+								<option value="1">
+									Horizontal
+								</option> 
+								<option value="2">
+									Vertical
+								</option>
+							</select>
+						</td>
+					</tr>
+					<tr class="table-danger">
+						<td>
+							5
+						</td>
+						<td>
+							Barco 5
+						</td>
+						<td>
+							<input name="nombredelacaja" size="2" type="text">
+							<input name="nombredelacaja" size="2" type="text">
+						</td>
+						<td>
+							<select name="OS">
+								 
+								<option value="1">
+									Horizontal
+								</option> 
+								<option value="2">
+									Vertical
+								</option>
+							</select>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="col-md-6">
+			<img alt="Bootstrap Image Preview" src="http://www.layoutit.com/img/sports-q-c-140-140-3.jpg">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<form role="form">
+				<div class="form-group">
+					 
+					<label for="exampleInputEmail1">
+						Email address
+					</label>
+					<input class="form-control" id="exampleInputEmail1" type="email">
+				</div>
+				<div class="form-group">
+				</div>
+				<div class="checkbox">
+				</div> 
+				<button type="submit" class="btn btn-primary">
+					Submit
+				</button>
+			</form>
+		</div>
+		<div class="col-md-6">
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
