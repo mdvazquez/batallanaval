1feec94917599c9bc7cd2927f8494a3078b66341
mdvazquez <maxiv_833@hotmail.com>
2018-05-02 21:09:19 -0300

    modularizando el footer con inclusion

--- header.php
+++ header.php
@@ -31,21 +31,10 @@
                 <div class="col-md-12">
                     <ul class="nav">
                         <li class="nav-item">
-                            <a class="nav-link active" href="#">Home</a>
+                            <a class="nav-link active" href="index.php">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Profile</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Messages</a>
-                        </li>
-                        <li class="nav-item dropdown ml-md-auto">
-                            <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-                                <div class="dropdown-divider">
-                                </div> <a class="dropdown-item" href="#">Separated link</a>
-                            </div>
+                            <a class="nav-link" href="paginajuego.php">Juego</a>
                         </li>
                     </ul>
                 </div>
